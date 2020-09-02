ec2f2a9dae20855f6ddfe27495abda7a8b89ba18
Pete Canfield <mistermcgreen@gmail.com>
2020-09-02 05:07:32 +0000

    Added runs

---
+++ BCCell/x86_64/mod_func.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "hocdec.h"
+extern int nrnmpi_myid;
+extern int nrn_nobanner_;
+
+extern void _h_reg(void);
+extern void _kdrBC_reg(void);
+extern void _leakBC_reg(void);
+extern void _natBC_reg(void);
+
+void modl_reg(){
+  if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+    fprintf(stderr, "Additional mechanisms from files\n");
+
+    fprintf(stderr," modfiles//h.mod");
+    fprintf(stderr," modfiles//kdrBC.mod");
+    fprintf(stderr," modfiles//leakBC.mod");
+    fprintf(stderr," modfiles//natBC.mod");
+    fprintf(stderr, "\n");
+  }
+  _h_reg();
+  _kdrBC_reg();
+  _leakBC_reg();
+  _natBC_reg();
+}
