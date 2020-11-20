a349880a7c0cf854c86550f400171ec87e07ac7b
Pete Canfield <mistermcgreen@gmail.com>
2020-11-20 00:47:41 -0600

    Actual SOM cell

--- cells/SOMCell/x86_64/mod_func.c
+++ cells/SOMCell/x86_64/mod_func.c
@@ -3,35 +3,35 @@
 extern int nrnmpi_myid;
 extern int nrn_nobanner_;
 
-extern void _OLM_IA_reg(void);
-extern void _OLM_Ih_reg(void);
-extern void _OLM_Lca_reg(void);
+extern void _nap_reg(void);
 extern void _OLM_cat_reg(void);
 extern void _OLM_ccanl_reg(void);
+extern void _OLM_IA_reg(void);
 extern void _OLM_ichan2_reg(void);
+extern void _OLM_Ih_reg(void);
+extern void _OLM_Lca_reg(void);
 extern void _OLM_sahp_reg(void);
-extern void _nap_reg(void);
 
 void modl_reg(){
   if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
     fprintf(stderr, "Additional mechanisms from files\n");
 
-    fprintf(stderr," modfiles//OLM_IA.mod");
-    fprintf(stderr," modfiles//OLM_Ih.mod");
-    fprintf(stderr," modfiles//OLM_Lca.mod");
+    fprintf(stderr," modfiles//nap.mod");
     fprintf(stderr," modfiles//OLM_cat.mod");
     fprintf(stderr," modfiles//OLM_ccanl.mod");
+    fprintf(stderr," modfiles//OLM_IA.mod");
     fprintf(stderr," modfiles//OLM_ichan2.mod");
+    fprintf(stderr," modfiles//OLM_Ih.mod");
+    fprintf(stderr," modfiles//OLM_Lca.mod");
     fprintf(stderr," modfiles//OLM_sahp.mod");
-    fprintf(stderr," modfiles//nap.mod");
     fprintf(stderr, "\n");
   }
-  _OLM_IA_reg();
-  _OLM_Ih_reg();
-  _OLM_Lca_reg();
+  _nap_reg();
   _OLM_cat_reg();
   _OLM_ccanl_reg();
+  _OLM_IA_reg();
   _OLM_ichan2_reg();
+  _OLM_Ih_reg();
+  _OLM_Lca_reg();
   _OLM_sahp_reg();
-  _nap_reg();
 }
